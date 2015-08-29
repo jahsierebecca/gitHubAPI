16dddc13cbfaf27193e9f1e9e95ed3d3853ae16e
Jahsie Ault <jahsieault@gmail.com>
2015-08-28 19:13:09 -0700

    Structure before express shift

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        options: {
+          outputStyle: 'compressed',
+          sourceMap: true,
+        },
+        files: {
+          'css/app.css': 'css/app.sass',
+        }
+      }
+    },
+
+    watch: {
+      grunt: {
+        options: {
+          reload: true
+        },
+        files: ['Gruntfile.js']
+      },
+
+      sass: {
+        files: 'css/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['build','watch']);
+}
---
+++ js/app.js
@@ -0,0 +1,77 @@
+var url = 'https://api.github.com/users/jahsierebecca';
+// var url = 'https://api.github.com/repos/CozyCo/slack-post/issues';
+
+//In lieu of using moment to sort things I just used the Github API sort option.
+var data = {
+	state: 'all',
+	sort: 'updated'
+}
+
+//Initiates the API call
+function go() {
+	$.ajax(url, { data: data, dataType: 'jsonp' })
+	.then(function(data, status, xhr) {
+		showIssues(data, status, xhr);
+		ko.applyBindings(viewModel);
+		console.log(data);
+		console.log(data.data.followers);
+	});
+}
+
+$(go);
+
+//Pulls the data from the call
+function showIssues(data,status,xhr) {
+	var info = data.data;
+	console.log(info);
+		var issue = new Issue(info);
+		viewModel.issues.push(issue);
+	// data.data.forEach(displayItem);
+}
+
+//Breaks the data into individual items. Sends them to Knockout.
+// function displayItem(item) {
+// 	// var issue = new Issue(item);
+// 	// viewModel.issues.push(issue);
+// }
+
+// Define the Issue class that will create each instance
+var Issue = function(issue) {
+	var user = issue;
+	if (user.bio === null) {
+		this.bio = "No user bio"
+	} else {
+		this.bio = user.bio;
+	}
+	if (user.company === null) {
+		this.company = "N/A"
+	} else {
+		this.company = user.company;
+	}
+	if (user.blog === null) {
+		this.blog = "N/A"
+	} else {
+		this.blog = user.blog;
+	}
+	this.username = user.login;
+	this.url = user.html_url;	
+	this.gravatar = user.avatar_url;
+    this.repos = user.public_repos;
+ 	this.followers = user.followers;
+ 	//In case of no description
+ 	// if (issue.body == "") {
+ 	// 	this.descrip = "No description provided";
+ 	// } else {
+ 	// 	this.descrip = issue.body;
+ 	// }
+ 	//Prep for add issue functionality
+ 	// console.log(issue)
+    // this.addIssue = function() {
+    //     this.info.push("New issue");
+    // }.bind(this);
+}
+ 
+//Model for KO to use to render things in the DOM
+var viewModel = {
+    issues: [],
+};
